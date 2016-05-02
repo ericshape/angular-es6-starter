b4a89567d8e25571dc0622046e18d5e2f2d3ccaa
ericshape <wangjiprc@gmail.com>
2016-05-01 23:21:01 -0400

    use controller to generate sign in page and sign up page.

--- src/js/auth/auth.config.js
+++ src/js/auth/auth.config.js
@@ -10,12 +10,14 @@ function AuthConfig($stateProvider, $httpProvider) {
 
         .state('app.login', {
             url: '/login',
+            controller: 'AuthCtrl as $ctrl',
             templateUrl: 'auth/auth.html',
             title: 'Sign in'
         })
 
         .state('app.register', {
             url: '/register',
+            controller: 'AuthCtrl as $ctrl',
             templateUrl: 'auth/auth.html',
             title: 'Sign up'
         });
